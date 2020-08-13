91430aac5419e7803ab1ce045504a647a803daf3
paddyrails <p_pillai3@hotmail.com>
2020-08-13 05:34:10 -0400

    small change in src/facts/js

--- src/facts.js
+++ src/facts.js
@@ -1,5 +1,5 @@
 import fetch from 'isomorphic-fetch';
-
+//comment made in test-branch
 export default function getFacts(){
     return fetch('https://ssr-react.firebaseio.com/facts.json')
         .then(res => res.json());
